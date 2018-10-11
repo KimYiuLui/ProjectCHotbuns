2f4dfc7e3d960da7171a3d1432b6d43197d2ad95
Imre Baas <imspons@gmail.com>
2018-10-11 13:23:15 +0200

    Update

--- routes/overige.js
+++ routes/overige.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/About", function (req, res) {
-    res.render("Klantenservice/About");
+router.get("/about", function (req, res) {
+    res.render("overige/about");
+})
+
+//exports every router so app.js can use these routes
+module.exports = router;
+
+
+router.get("/veelgesteldevragen", function (req, res) {
+    res.render("overige/veelgesteldevragen");
 })
 
 //exports every router so app.js can use these routes
