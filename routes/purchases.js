51102b78e39ada7d022ce5354104976aec69b7f9
ImreBaas <imspons@gmail.com>
2018-10-13 20:14:01 +0200

    Shoppingcart + Koop Pagina
    Kleine verandering bij de producte pagina is er ook. (Knop kleur is anders)

--- routes/purchases.js
+++ routes/purchases.js
@@ -1,12 +1,32 @@
 var express = require("express");
 var router = express.Router();
+var Product = require("../models/product");
 
-
-////TEST////
+////TEST//// ZORG ERVOOR DAT HET ALLE ITEMS PAKT DIE IN WINKELWAGEN ZITTEN
 router.get("/shoppingcart", function (req, res) {
-    res.render("purchases/shoppingcart");
+    Product.find({ category: "koek" }, function (error, allBrood) {
+        if (error) {
+            console.log(error)
+            res.redirect("/")
+        }
+        else {
+            res.render("purchases/shoppingcart", { product: allBrood });
+        }
+    });
 })
-////TEST////
+
+router.get("/purchase", function (req, res) {
+    Product.find({ category: "koek" }, function (error, allBrood) {
+        if (error) {
+            console.log(error)
+            res.redirect("/")
+        }
+        else {
+            res.render("purchases/purchase", { product: allBrood });
+        }
+    });
+})
+////TEST//// ZORG ERVOOR DAT HET ALLE ITEMS PAKT DIE IN WINKELWAGEN ZITTEN
 
 //exports every router so app.js can use these routes
 module.exports = router;
