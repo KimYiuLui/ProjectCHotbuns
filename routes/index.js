46290907e46240a340801965c1d1f0f7b0d00360
ImreBaas <imspons@gmail.com>
2018-11-18 10:49:41 +0100

    Confirm Password + Register gaat niet leeg

--- routes/index.js
+++ routes/index.js
@@ -14,29 +14,35 @@ router.get("/", function(req, res){
 // Register and login
 //-------------------------------------
 router.get("/signup", function(req, res){
-    res.render("signup");
+    res.render("signup", { AttemptedRegister: 0 })
 });
 
 router.post("/signup", function (req, res) {
     var newName = req.body.name.slice(0, -1); // Ducktape voor een bug.
-    User.register(new User({username: req.body.username,
-                            role: "Admin",
-                            email: req.body.email,
-                            name: newName,
-                            naamToevoeging: req.body.naamToevoeging,
-                            surname: req.body.surname,
-                            phonenumber: req.body.phonenumber,
-                            address: req.body.address
-    }), req.body.password, function(error, user){
-       if(error){
-           console.log(error);
-           req.flash("error", "Gebruikersnaam en/of e-mail adres bestaat al. Gebruik een andere gebruikersnaam of e-mail adres.");
-           res.redirect("/signup");
-       }
-       passport.authenticate("local")(req, res, function(){
-          res.redirect("/"); 
-       });
-    });
+    if (req.body.password == req.body.confirm_password) {
+        User.register(new User({
+            username: req.body.username,
+            role: "Admin",
+            email: req.body.email,
+            name: newName,
+            naamToevoeging: req.body.naamToevoeging,
+            surname: req.body.surname,
+            phonenumber: req.body.phonenumber,
+            address: req.body.address
+        }), req.body.password, function (error, user) {
+            if (error) {
+                console.log(error);
+                req.flash("error", "Gebruikersnaam en/of e-mail adres bestaat al. Gebruik een andere gebruikersnaam of e-mail adres.");
+                res.render("signup", { AttemptedRegister: 1, email: req.body.email, name: req.body.name, surname: req.body.surname, Toevoeging: req.body.naamtoevoeging, phonenumber: req.body.phonenumber, city: req.body.address.city, street: req.body.address.street, zipcode: req.body.address.zipcode, housenumber: req.body.address.number, username: req.body.username })
+            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/");
+            });
+        });
+    } else {
+        req.flash("error", "De wachtwoorden zijn niet gelijk");
+        res.render("signup", { AttemptedRegister: 1, email: req.body.email, name: req.body.name, surname: req.body.surname, Toevoeging: req.body.naamtoevoeging, phonenumber: req.body.phonenumber, city: req.body.address.city, street: req.body.address.street, zipcode: req.body.address.zipcode, housenumber: req.body.address.number, username: req.body.username })
+    }
 });
 
 
