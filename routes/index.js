2a742dbb1187946ceec2d58fa777393adc60adff
KimYiuLui <kimyiu-lui@hotmail.com>
2018-11-26 20:35:12 +0100

    added Verification token to activate new accounts

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var User = require("../models/user");
 var passport = require("passport");
 var nodemailer = require('nodemailer');
+var host, rand, link, emailHtml, mailOptions;
 
 //-------------------------------------
 //Home and other informative pages 
@@ -22,8 +23,9 @@ router.post("/signup", function (req, res) {
     var newName = req.body.name.slice(0, -1); // Ducktape voor een bug.
     if (req.body.password == req.body.confirm_password) {
         User.register(new User({
+            active: false,
             username: req.body.username,
-            role: "User",
+            role: "Admin",
             email: req.body.email,
             name: newName,
             naamToevoeging: req.body.naamToevoeging,
@@ -46,15 +48,19 @@ router.post("/signup", function (req, res) {
                 });
 
                 var fullaname;
-                if(req.body.naamToevoeging != null){
+                if (req.body.naamToevoeging != null) {
                     fullaname = newName + " " + req.body.naamToevoeging + " " + req.body.surname;
                 }
-                else{
+                else {
                     fullaname = newName + " " + req.body.surname;
                 }
 
-                var emailHtml = "<a> Beste " +  fullaname + ",<a> <br /><br /> <a> Welkom bij Hotbuns. Hierbij bevestigen wij dat uw account succesvol is aangemaakt.<br /><br /><a>Met vriendelijke groet,<a/><br/><br/> <a>HotBunsJs<a>"
-                var mailOptions = {
+                rand = Math.floor((Math.random() * 100 + 54));
+                host = req.get('host')
+                link = "http://" + host + "/verify?id=" + rand;
+
+                emailHtml = "<a> Beste " + fullaname + ",<a> <br /><br /> <a> Welkom bij Hotbuns. Hierbij bevestigen wij dat uw account succesvol is aangemaakt. <br/><a href=" + link + ">Klik deze link om je email te bevestigen<a/><br /><br /><a>Met vriendelijke groet,<a/><br/><br/> <a>HotBunsJs<a>"
+                mailOptions = {
                     from: 'hotbunsemail@gmail.com',
                     to: req.body.email,
                     subject: 'Account bevestiging',
@@ -78,6 +84,36 @@ router.post("/signup", function (req, res) {
     }
 });
 
+router.get('/verify', function (req, res) {
+    console.log(req.protocol + ":/" + req.get('host'));
+    // if((req.protocol+"://"+req.get('host'))==("http://"+host))
+    if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
+        console.log("Domain is matched. Information is from Authentic email");
+        if (req.query.id == rand) {
+            User.findOne({ email: mailOptions.to }, function (error, foundUser) {
+                if (error) {
+                    console.log(error)
+                }
+                else {
+                    foundUser.active = true;
+                    foundUser.save();
+                }
+            })
+            console.log("email is verified")
+            req.flash("success", "Uw account is nu geactiveerd.");
+            res.redirect("/login")
+        }
+        else {
+            console.log("email is not verified");
+            req.flash("error", "Er is iets misgegaan tijdens het activeringsproces. Neem contact op met hotbunsemail@gmail.com");
+            res.redirect("/login")
+        }
+    }
+    else {
+        req.flash("error", "Er is iets misgegaan tijdens het activeringsproces. Neem contact op met hotbunsemail@gmail.com");
+        res.redirect("/login")
+    }
+})
 
 router.get("/login", function (req, res) {
     res.render("login");
@@ -86,11 +122,20 @@ router.get("/login", function (req, res) {
 
 router.post("/login", passport.authenticate("local",
     {
-        successRedirect: "/",
         failureRedirect: "/login",
         failureFlash: "Uw inlog gegevens zijn incorrect. Probeer het nog een keer."
-    }), function (req, res) {
-    })
+    }), (req, res) => {
+        if(req.user.active === true){
+            res.redirect("/");
+        }
+        if(req.user.active === false){
+            req.flash('error', 'Uw account is nog niet geactiveerd. Controleer uw email om je account te activeren.');
+            res.redirect("/login");
+        }
+        req.flash('error', 'Er is iets misgegaan probeer het nog een keer.');
+        res.redirect("/login");  
+})
+
 
 //log out the user
 router.get("/logout", function (req, res) {
