3df9703e60924fb8b1f189883eb328c49fac5c05
ImreBaas <imspons@gmail.com>
2018-12-09 00:09:21 +0100

    Final touches
    --Opmaak en zoekfuncties verbeterd.

--- app.js
+++ app.js
@@ -74,9 +74,11 @@ app.use(FavoriteRoutes);
 // });
 
 
+
 //setting for local debug at local host 3000
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
+
