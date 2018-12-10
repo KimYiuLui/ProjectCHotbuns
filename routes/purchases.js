f90b0685a068052ec9c43cc400eeb4c3096ddec6
Imre Baas <imspons@gmail.com>
2018-12-10 13:28:04 +0100

    Test chart.js

--- routes/purchases.js
+++ routes/purchases.js
@@ -139,6 +139,7 @@ router.post("/purchase/order", function (req, res) {
         orderedProductsName: req.body.name
     }));
 
+
 // Voor de email opmaak. Zet alles onder elkaar.
     var notArray = []
     noLayout = req.body.name
