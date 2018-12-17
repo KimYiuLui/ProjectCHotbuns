4a98d2d194e6c98b1334eb91dcd060d0e80b8f6b
ImreBaas <imspons@gmail.com>
2018-12-17 16:14:12 +0100

    Statistieken Filters + Bestelling opzoeken
    --Statistieken naar aparte pagina
    --Filters voor de statistieken
    --Bestellingen kan je nu opzoeken

--- routes/stats.js
+++ routes/stats.js
@@ -16,40 +16,64 @@ router.get("/stats/", isLoggedIn, function (req, res) {
     });
 });
 
+
 router.post("/stats/filter", isLoggedIn, function (req, res) {
-    if (req.body.Option == "All") {
-        Product.find({}).sort({ amountbought: 'descending' }).exec(function (err, product) {
-            if (err) {
-                console.log(err)
-            }
-            res.render("stats/productgraphs", { result: product, currentCategory: "all" })
-        })
-    };
-    if (req.body.Option == "Brood") {
-        Product.find({category: 'brood'}).sort({ amountbought: 'descending' }).exec(function (err, product) {
-            if (err) {
-                console.log(err)
-            }
-            res.render("stats/productgraphs", { result: product, currentCategory: "brood" })
-        })
-    };
-    if (req.body.Option == "Koek") {
-        Product.find({category: 'koek'}).sort({ amountbought: 'descending' }).exec(function (err, product) {
-            if (err) {
-                console.log(err)
-            }
-            res.render("stats/productgraphs", { result: product, currentCategory: "koek" })
-        })
-    };
-    if (req.body.Option == "Zoetigheid") {
-        Product.find({category: 'zoetigheid'}).sort({ amountbought: 'descending' }).exec(function (err, product) {
-            if (err) {
-                console.log(err)
-            }
-            res.render("stats/productgraphs", { result: product, currentCategory: "zoetigheid"})
-        })
-    };
+    var query = Product.find();
+    var selection = req.body.Category;
+
+    console.log("HUIDIGE CATEGORIE = " + req.body.Category)
+    console.log("HUIDIGE OPTIE = " + req.body.Option)
+    console.log("HUIDIGE FILTER = " + req.body.Filter)
+    console.log("---------------------------------------")
+
+    // Welke productegroep is gekozen
+    if (req.body.Option == "Brood" || req.body.Category == "brood") {
+        query.where('category').equals('brood');
+    }
+    else if (req.body.Option == "Koek" || req.body.Category == "koek") {
+        query.where('category').equals('koek');
+    }
+    else if (req.body.Option == "Zoetigheid" || req.body.Category == "zoetigheid") {
+        query.where('category').equals('zoetigheid');
+    }
+    //Welke filter is gekozen
+    if (req.body.Filter == "0") {
+        query.where('amountbought').equals(0);
+    }
+    else if (req.body.Filter == "1+") {
+        query.where('amountbought').gt(0);
+    }
+    else if (req.body.Filter == "50+") {
+        query.where('amountbought').gt(50);
+    }
+    else if (req.body.Filter == "100+") {
+        query.where('amountbought').gt(100);
+    }
+
+
+
+
+    //Executeer de query
+    query.sort({ amountbought: 'descending' });
+    query.exec().then(product => {
+
+        res.render("stats/productgraphs", { result: product, currentCategory: selection })
+    });
 });
+
+router.post("/stats/orderFind", isLoggedIn, function (req, res) {
+    var Filter = req.body.productName
+    console.log("HUIDIGE PRODUCT = " + req.body.productName)
+    Order.find({ orderedProductsName: { $regex: Filter } }).exec(function (err, result) {
+        if (err) {
+            console.log(err)
+        }
+        console.log("HUIDIGE RESULTAAT = " + result)
+        res.render("stats/orderTable", { order: result })
+        
+    });
+});
+
 
 
 router.put("/stats/resetProduct", function (req, res) {
@@ -62,12 +86,6 @@ router.put("/stats/resetProduct", function (req, res) {
         }
     });
 });
-
-
-
-
-
-
 
 
 function isLoggedIn(req, res, next) {
